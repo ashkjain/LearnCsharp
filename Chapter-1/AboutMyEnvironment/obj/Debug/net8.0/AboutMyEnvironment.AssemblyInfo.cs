7dbfc928b213249b9f906f9df43e0e400caaa9cd
ashishjain-repo <ashishjain99@yahoo.com>
2024-04-07 16:40:06 -0600

    Added Project Named Variables

--- Chapter-1/AboutMyEnvironment/obj/Debug/net8.0/AboutMyEnvironment.AssemblyInfo.cs
+++ Chapter-1/AboutMyEnvironment/obj/Debug/net8.0/AboutMyEnvironment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AboutMyEnvironment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a1054cba9cbdc8a56f3c2d414c80043cea5d48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a01527998cf961a0eea23726cbdcd8e39ebfea")]
 [assembly: System.Reflection.AssemblyProductAttribute("AboutMyEnvironment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AboutMyEnvironment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Chapter-2/Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double heightInMeters = 1.88;
+        Console.WriteLine($"The Variable {nameof(heightInMeters)} has the {heightInMeters}");
+    }
+}
